bf7951d00080ff0a15fab1c8fae112f81b236e35
Alp9ne <dystopiaisunavoidable@gmail.com>
2023-04-16 15:55:30 +0800

    refactor js function to create grids, through rows and sub divs.

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-function createGrids() {
+/* function to create a div within another div (LATER) */
+
+function createGrids(userInput) {
     const page = document.querySelector('body');
     const mainDiv = document.createElement('div');
-    mainDiv.classList.add('the-big-div');
-
-    mainDiv.style.width = '960px';
-    mainDiv.style.height = '960px';
+    mainDiv.classList.add('main-div');
+    const height = 960;
+    mainDiv.style.height = `${height}px`;
     page.appendChild(mainDiv);
+
+
+    const minHeight = height/userInput;
+    let i = 0;
+    while(i < userInput) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.style.maxHeight = `${minHeight}px`
+        mainDiv.appendChild(row);
+        i++;
+    }
+
+    const rows = document.querySelectorAll('.row');
+    rows.forEach((item) => {
+        let k = 0;
+        while (k < userInput) {
+            const subDiv = document.createElement('div');
+            subDiv.classList.add('sub-div');
+            subDiv.style.height = `${minHeight}px`;
+            item.appendChild(subDiv);
+            k++;
+        }
+    });
+    
 }
 
 let gridInput = 16; // change to take manual user input later.
-createGrids();
+createGrids(gridInput);
